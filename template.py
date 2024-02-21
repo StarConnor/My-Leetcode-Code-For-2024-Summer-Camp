7af0a7bd96421c9cc87b406924f723d39515119c
Fukang <carryon2021@163.com>
2024-02-21 19:35:44 +0800

    update

---
+++ template.py
@@ -0,0 +1,9 @@
+class Solution:
+    def solv(arg1):
+        pass
+
+if __name__ == "__main__":
+    solution = Solution()
+    arg1 = None
+    print(f"Input:{arg1}")
+    print(f"Result:{solution.solv(arg1)}")
